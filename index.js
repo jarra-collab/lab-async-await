a113670bb2e01614213b3bbbcf63c2439633a626
abdiaziz <abdiazizabdullahi40@gmail.com>
2026-01-07 11:16:39 -0800

    lab-async-await

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+async function fetchPosts() {
+  const postList = document.getElementById("post-list");
+  if (!postList) return;
+
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+
+  const post = posts[0];
+
+  postList.innerHTML =
+    "<h1>" + post.title + "</h1>" +
+    "<p>" + post.body + "</p>";
+}
+
+fetchPosts();
+
+
+
+
